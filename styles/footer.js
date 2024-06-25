1f7af81dcc80743e200b1288c562ce43a58e7de8
Branden21 <statefan1324@gmail>
2024-06-25 15:41:23 -0400

    email button to my email address

--- styles/footer.js
+++ styles/footer.js
@@ -22,6 +22,10 @@ function footer(){
                         <a href="https://www.linkedin.com/in/branden-husted-6901aa211/" target="_blank" class="fa fa-linkedin"></a>\
                     
                         <a href="https://www.instagram.com/brandenhusted21/" target="_blank" class="fa fa-instagram"></a>\
+
+                        
+                        <button onclick="window.location.href='mailto:statefan1324@gmail.com'">Send me an email</button>
+        
                       
                     </div>\
                     
